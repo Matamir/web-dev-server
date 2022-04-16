823d9048178597fef04aadb1f489e08670276c48
Matamir <mhirsch217@gmail.com>
2022-04-16 00:00:33 -0400

    a8 MONGO sp22

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,5 +1,4 @@
-import posts from "./tuits/tuits.js";
-let tuits = posts;
+import tuitsDao from "../../tuits/tuits-dao.js";
 
 export default (app) => {
   app.post('/api/tuits', createTuit);
@@ -8,29 +7,28 @@ export default (app) => {
   app.delete('/api/tuits/:tid', deleteTuit);
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
   const newTuit = req.body;
-  newTuit._id = (new Date()).getTime()+'';
-  newTuit.likes = 0;
-  tuits.push(newTuit);
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
   res.json(newTuit);
 }
 
-const findAllTuits = (req, res) => {
+const findAllTuits = async (req, res) => {
+  const tuits = await tuitsDao.findAllTuits()
   res.json(tuits);
 }
 
 const updateTuit = (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updatedTuit = req.body;
-  tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
-  res.sendStatus(200);
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
+  res.send(status);
 }
 
 const deleteTuit = (req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-  res.sendStatus(200);
+  const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+  res.send(status);
 }
 
 
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
+import tuitsController from "./tuits/tuits-controller.js"
 app.use(cors());
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
